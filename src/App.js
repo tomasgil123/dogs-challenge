102a82f8f438d04ee4ffbb3423c8c064419a79f9
Tomas Gil <tomasgil1234@gmail.com>
2021-07-07 09:00:14 -0300

    feat: add breed photos display functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg'
 import './App.css'
 import React, { useEffect, useState } from 'react'
 
+// components
+import Pages from './components/pages'
+
 function App() {
   const [listBreeds, setListBreeds] = useState([])
   const [search, setSearch] = useState('')
@@ -18,6 +21,8 @@ function App() {
       // si el breed tiene mas de un sub-breed vamos a agregar cada sub-breed
       // con el objetivo de lograr para bulldopg tener 3 opciones
       // bulldog-boston -- bulldog-english -- bulldog-french
+      // es decir, vamos a tratar cada sub-breed como otro breed
+      // van a estar al mismo nivel que un breed en la lista
       if (allBreedsMessage.message[breed].length > 1) {
         allBreedsMessage.message[breed].forEach((subBreed) => {
           allBreeds.push(`${breed}-${subBreed}`)
@@ -33,6 +38,24 @@ function App() {
     getAllBreeds()
   }, [])
 
+  const [selectedBreed, setSelectedBreed] = useState('')
+  const [breedImages, setBreedImages] = useState([])
+
+  const getImagesBreed = async () => {
+    const breed = selectedBreed.replace('-', '.')
+    const breedImagesResponse = await fetch(
+      `${process.env.REACT_APP_BASE_URL}breed/${breed}/images`
+    )
+    const breedImages = await breedImagesResponse.json()
+    setBreedImages(breedImages)
+  }
+
+  useEffect(() => {
+    if (selectedBreed) {
+      getImagesBreed()
+    }
+  }, [selectedBreed])
+
   return (
     <div>
       <div>
@@ -43,12 +66,20 @@ function App() {
             {listBreeds
               .filter((breed) => breed.includes(search))
               .map((breed) => (
-                <div key={breed}>{breed}</div>
+                <div key={breed} onClick={() => setSelectedBreed(breed)}>
+                  {breed}
+                </div>
               ))}
           </div>
         ) : (
           <div>Start to type something to see the list of dogs</div>
         )}
+        <div>
+          <h1>Dogs in breed</h1>
+          <div>
+            <Pages totalImages={breedImages.length} />
+          </div>
+        </div>
       </div>
     </div>
   )
